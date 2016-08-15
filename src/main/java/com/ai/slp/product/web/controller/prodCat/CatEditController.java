ace4e8d5621e3b82071206636fcac42847aaf709
liutong <liutong6053@163.com>
2016-08-15 09:58:21 +0800

    完善类目添加功能.

--- src/main/java/com/ai/slp/product/web/controller/prodCat/CatEditController.java
+++ src/main/java/com/ai/slp/product/web/controller/prodCat/CatEditController.java
@@ -1,15 +1,27 @@
 package com.ai.slp.product.web.controller.prodCat;
 
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
+import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
+import com.ai.opt.sdk.util.BeanUtils;
 import com.ai.opt.sdk.web.model.ResponseData;
+import com.ai.slp.product.api.productcat.interfaces.IProductCatSV;
+import com.ai.slp.product.api.productcat.param.ProductCatParam;
+import com.ai.slp.product.web.constants.SysCommonConstants;
 import com.ai.slp.product.web.model.prodCat.ProdCatInfo;
+import com.ai.slp.product.web.util.AdminUtil;
 import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +30,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/cat/edit")
 public class CatEditController {
+    private static Logger logger = LoggerFactory.getLogger(CatEditController.class);
 
     /**
      * 显示添加页面
@@ -36,12 +49,28 @@ public class CatEditController {
      */
     @RequestMapping(value = "/create",method = RequestMethod.POST)
     @ResponseBody
-    public ResponseData<String> addCat(String catListStr){
+    public ResponseData<String> addCat(String catListStr, HttpSession session){
         ResponseData<String> responseData = new ResponseData<String>(
                 ResponseData.AJAX_STATUS_SUCCESS, ExceptCodeConstants.Special.SUCCESS);
         List<ProdCatInfo> catInfoList = JSON.parseArray(catListStr,ProdCatInfo.class);
+        List<ProductCatParam> catParamList = new ArrayList<>();
         for (ProdCatInfo catInfo:catInfoList){
+            ProductCatParam catParam = new ProductCatParam();
             System.out.println(catInfo.getProductCatName()+":"+catInfo.getParentProductCatId());
+            BeanUtils.copyProperties(catParam,catInfo);
+            catParam.setTenantId(SysCommonConstants.COMMON_TENANT_ID);
+            catParam.setOperId(AdminUtil.getAdminId(session));
+            catParamList.add(catParam);
+        }
+        IProductCatSV productCatSV = DubboConsumerFactory.getService(IProductCatSV.class);
+        BaseResponse response = productCatSV.createProductCat(catParamList);
+        ResponseHeader header = response==null?null:response.getResponseHeader();
+        if (header==null || !header.isSuccess()){
+            String errorCode = header==null?ExceptCodeConstants.Special.SYSTEM_ERROR:header.getResultCode();
+            String errMsg = header==null?"未知错误":header.getResultMessage();
+            logger.error("Create cat is error,errorCode:{},errorMsg:{}",errorCode,errMsg);
+            responseData = new ResponseData<String>(
+                    ResponseData.AJAX_STATUS_FAILURE, errorCode,errMsg);
         }
         return responseData;
     }
