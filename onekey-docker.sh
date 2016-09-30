31f680172afe93a7085262e0d1dc37aaecd4b597
gucl <gucl@asiainfo.com>
2016-09-30 16:20:30 +0800

    onekey 文件格式调整

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -1,4 +1,4 @@
-git pull
-gradle clean && gradle build -x test
+git pull 
+gradle clean && gradle build -x test 
 docker build -t 10.19.13.18:5000/slp-product-web:v1.0 .   
-docker push 10.19.13.18:5000/slp-product-web:v1.0
+docker push 10.19.13.18:5000/slp-product-web:v1.0 
