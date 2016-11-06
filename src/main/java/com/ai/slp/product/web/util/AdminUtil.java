2b8eb8eff6da342fe84e1c2a63d7f5594ef72e9a
jiawen <wen6485@139.com>
2016-11-06 12:11:55 +0800

    修改supplier_id为-1

--- src/main/java/com/ai/slp/product/web/util/AdminUtil.java
+++ src/main/java/com/ai/slp/product/web/util/AdminUtil.java
@@ -51,6 +51,6 @@ public class AdminUtil {
      */
     public static String getSupplierId(){
 //        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
-        return "2000";
+        return "-1";
     }
 }
